c40719a5dd4db3911113b73dcaa1c38a24b8eb03
sandokan <sandokandias@gmail.com>
2022-05-24 13:37:59 -0300

    refactor

--- grammars-java/junit_migraton.py
+++ grammars-java/junit_migraton.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import antlr4
 from antlr4 import ParseTreeWalker
 from antlr4.TokenStreamRewriter import TokenStreamRewriter
@@ -11,20 +13,26 @@ def __parse(code):
     tokens = antlr4.CommonTokenStream(lexer)
     parser = JavaParser(tokens)
     return tokens, parser.compilationUnit()
-                
+
+def __migrate(project_path):
+    for dirpath, _, files in os.walk(os.path.join(project_path, "src/test")):
+            for file in files:
+                src = open(os.path.join(dirpath, file), "r").read()
+                tokens, tree = __parse(src)
+                rewriter = TokenStreamRewriter(tokens)    
+                default_listener = junit_listeners.DefaultListener(rewriter)
+                ParseTreeWalker().walk(default_listener, tree)
+                remainder_imports_listener = junit_listeners.ReminderImportsListener(rewriter, default_listener.remaining_imports)
+                ParseTreeWalker().walk(remainder_imports_listener, tree)
+                newcode = remainder_imports_listener.getParsedSourceAsText()
+                print(newcode)
+                __parse(newcode)
+
                 
 def main():
-    code = open("./sample-app/src/test/java/com/example/project/JUnit4Test.java", "r").read()
-    tokens, tree = __parse(code)
-    rewriter = TokenStreamRewriter(tokens)    
-    default_listener = junit_listeners.DefaultListener(rewriter)
-    ParseTreeWalker().walk(default_listener, tree)
-    remainder_imports_listener = junit_listeners.ReminderImportsListener(rewriter, default_listener.remaining_imports)
-    ParseTreeWalker().walk(remainder_imports_listener, tree)
-    newcode = remainder_imports_listener.getParsedSourceAsText()
-    print(newcode)
-    __parse(newcode)
-
+    project_path = sys.argv[1]
+    __migrate(project_path)
+    
 
 if __name__ == '__main__':
     main()
